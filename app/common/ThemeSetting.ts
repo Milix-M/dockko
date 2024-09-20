8e1b9f2d41be1675543dd529df765c1e93789a8f
Milix-M <tujikou12dh@gmail.com>
2024-09-20 11:32:23 +0900

    テーマ設定を追加

---
+++ app/common/ThemeSetting.ts
@@ -0,0 +1,19 @@
+export const theme = {
+    list: {
+        styles: {
+            base: {
+                item: {
+                    initial: {
+                        bg: "hover:bg-blue-gray-50 hover:bg-opacity-80",
+                        color: "hover:text-blue-gray-900 focus:text-blue-gray-900",
+                    },
+                    selected: {
+                        bg: "bg-teal-100/50",
+                        color: "text-blue-gray-700",
+                        fontWeight: "font-semibold"
+                    },
+                },
+            },
+        },
+    },
+};
