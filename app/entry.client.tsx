372765adaa6f0535a9a8d9ff3b5f5e4c9ddfdd48
Milix-M <tujikou12dh@gmail.com>
2024-09-20 11:32:50 +0900

    ThemeProviderが正しく動作していなかったのを修正

--- app/entry.client.tsx
+++ app/entry.client.tsx
@@ -2,14 +2,15 @@ import { ThemeProvider } from "@material-tailwind/react";
 import { RemixBrowser } from "@remix-run/react";
 import { startTransition, StrictMode } from "react";
 import { hydrateRoot } from "react-dom/client";
+import { theme } from "./common/ThemeSetting";
 
 startTransition(() => {
   hydrateRoot(
     document,
     <StrictMode>
-      <ThemeProvider>
+      <ThemeProvider value={theme}>
         <RemixBrowser />
       </ThemeProvider>
-    </StrictMode>
+    </StrictMode >
   );
 });
