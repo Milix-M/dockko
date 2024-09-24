faae6f116e70717b81bfa91736c04e8cf256d4d3
Milix-M <tujikou12dh@gmail.com>
2024-09-24 10:31:08 +0900

    ContainerDetails型を追加

--- app/common/interfaces.ts
+++ app/common/interfaces.ts
@@ -51,3 +51,184 @@ export interface container {
         "Propagation": string
     }]
 }
+
+export interface ContainerDetails {
+    "AppArmorProfile": string
+    "Args": string[]
+    "Config": {
+        "AttachStderr": boolean
+        "AttachStdin": boolean
+        "AttachStdout": boolean
+        "Cmd": string[]
+        "Domainname": string
+        "Env": string[]
+        "Healthcheck": {
+            "Test": string[]
+        }
+        "Hostname": string
+        "Image": string
+        "Labels": {
+            string: string
+        }
+        "MacAddress": string
+        "NetworkDisabled": boolean
+        "OpenStdin": boolean
+        "StdinOnce": boolean
+        "Tty": boolean
+        "User": string
+        "Volumes": {
+            string: { string: string }
+        },
+        "WorkingDir": string
+        "StopSignal": string
+        "StopTimeout": number
+    },
+    "Created": number
+    "Driver": string
+    "ExecIDs": string[]
+    "HostConfig": {
+        "MaximumIOps": number
+        "MaximumIOBps": number
+        "BlkioWeight": number
+        "BlkioWeightDevice": [
+            { string: string }
+        ]
+        "BlkioDeviceReadBps": [
+            { string: string }
+        ]
+        "BlkioDeviceWriteBps": [
+            { string: string }
+        ]
+        "BlkioDeviceReadIOps": [
+            { string: string }
+        ]
+        "BlkioDeviceWriteIOps": [
+            { string: string }
+        ]
+        "ContainerIDFile": string
+        "CpusetCpus": string
+        "CpusetMems": string
+        "CpuPercent": number
+        "CpuShares": number
+        "CpuPeriod": number
+        "CpuRealtimePeriod": number
+        "CpuRealtimeRuntime": number
+        "Devices": string[]
+        "DeviceRequests": [
+            {
+                "Driver": string
+                "Count": number
+                "DeviceIDs\"": string[]
+                "Capabilities": [
+                    string[]
+                ],
+                "Options": {
+                    string: string
+                }
+            }
+        ]
+        "IpcMode": string
+        "Memory": number
+        "MemorySwap": number
+        "MemoryReservation": number
+        "OomKillDisable": boolean
+        "OomScoreAdj": number
+        "NetworkMode": string
+        "PidMode": string
+        "PortBindings": { string: string }
+        "Privileged": boolean
+        "ReadonlyRootfs": boolean
+        "PublishAllPorts": boolean
+        "RestartPolicy": {
+            "MaximumRetryCount": number
+            "Name": string
+        },
+        "LogConfig": {
+            "Type": string
+        },
+        "Sysctls": {
+            "net.ipv4.ip_forward": string
+        },
+        "Ulimits": [
+            { string: string }
+        ],
+        "VolumeDriver": string
+        "ShmSize": number
+    },
+    "HostnamePath": string
+    "HostsPath": string
+    "LogPath": string
+    "Id": string
+    "Image": string
+    "MountLabel": string
+    "Name": string
+    "NetworkSettings": {
+        "Bridge": string
+        "SandboxID": string
+        "HairpinMode": boolean
+        "LinkLocalIPv6Address": string
+        "LinkLocalIPv6PrefixLen": number
+        "SandboxKey": string
+        "EndpointID": string
+        "Gateway": string
+        "GlobalIPv6Address": string
+        "GlobalIPv6PrefixLen": number
+        "IPAddress": string
+        "IPPrefixLen": number
+        "IPv6Gateway": string
+        "MacAddress": string
+        "Networks": {
+            "bridge": {
+                "NetworkID": string
+                "EndpointID": string
+                "Gateway": string
+                "IPAddress": string
+                "IPPrefixLen": number
+                "IPv6Gateway": string
+                "GlobalIPv6Address": string
+                "GlobalIPv6PrefixLen": number
+                "MacAddress": string
+            }
+        }
+    },
+    "Path": string
+    "ProcessLabel": string
+    "ResolvConfPath": string
+    "RestartCount": number
+    "State": {
+        "Error": string
+        "ExitCode": number
+        "FinishedAt": string
+        "Health": {
+            "Status": string
+            "FailingStreak": number
+            "Log": [
+                {
+                    "Start": string
+                    "End": string
+                    "ExitCode": number
+                    "Output": string
+                }
+            ]
+        },
+        "OOMKilled": false
+        "Dead": false
+        "Paused": false
+        "Pid": number
+        "Restarting": false
+        "Running": true
+        "StartedAt": string
+        "Status": string
+    },
+    "Mounts": [
+        {
+            "Name": string
+            "Source": string
+            "Destination": string
+            "Driver": string
+            "Mode": string
+            "RW": false
+            "Propagation": string
+        }
+    ]
+}
