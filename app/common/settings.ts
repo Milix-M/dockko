2002f0361698c2c040d3a4e1dbf41d32755594ab
Milix-M <tujikou12dh@gmail.com>
2024-09-25 17:43:30 +0900

    settingsLoaderをsettingsに改名

--- app/common/settings.ts
+++ app/common/settings.ts
@@ -3,7 +3,7 @@
  * @param key 読み出したいキー
  * @returns 設定値: string, 指定したキーに対応する値が無い場合undefined
  */
-export default function loadSettingsValue(key: string | undefined | null) {
+export function loadSettingsValue(key: string | undefined | null) {
     // null or undefinedなら早期リターン
     if (key == null) {
         return undefined
@@ -17,5 +17,19 @@ export default function loadSettingsValue(key: string | undefined | null) {
         return undefined
     } else {
         return value
+    }
+}
+
+/**
+ * 接続設定のデータをLocal Storageに保存する
+ * @param serverURL "dockerEngineServer"キーで保存する値
+ * @param apiVersion "dockerApiVersion"キーで保存する値
+ */
+export function handleSave(serverURL: string | undefined, apiVersion: string | undefined) {
+    if (serverURL != null) {
+        localStorage.setItem("dockerEngineServer", serverURL);
+    }
+    if (apiVersion != null) {
+        localStorage.setItem("dockerApiVersion", apiVersion);
     }
 }
