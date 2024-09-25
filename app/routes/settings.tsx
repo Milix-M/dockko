89ece5a89ffb5ae73a2692180f25c0d385fc7a48
Milix-M <tujikou12dh@gmail.com>
2024-09-25 17:09:10 +0900

    デザイン調整, 保存ボタン追加, 説明追加

--- app/routes/settings.tsx
+++ app/routes/settings.tsx
@@ -1,4 +1,4 @@
-import { Card, CardHeader, Input } from "@material-tailwind/react"
+import { Button, Card, CardBody, CardFooter, CardHeader, Input } from "@material-tailwind/react"
 import { useLoaderData } from "@remix-run/react"
 import electron from "~/electron.server"
 
@@ -22,20 +22,27 @@ export default function Settings() {
                 >
                     <h2 className="font-bold text-xl text-blue-gray-900">接続設定</h2>
                 </CardHeader>
-                <div className="mt-2 px-6 py-4">
+                <CardBody>
                     <div className="flex justify-center items-center w-full gap-6">
                         <div className="w-full">
                             <span className="font-semibold text-md mb-1 block text-blue-gray-900">Docker Engine Server</span>
+                            <span className="text-xs mb-2 block">DockerエンジンのURLを指定します。<br />現在Unix Socketには対応していないためHTTP接続での接続になります。</span>
                             <Input
                                 label="Server URL" placeholder="http://127.0.0.1:2375" value={"http://127.0.0.1:2375"} crossOrigin={undefined} />
                         </div>
                         <div className="w-full">
                             <span className="font-semibold text-md mb-1 block text-blue-gray-900">Docker API Version</span>
+                            <span className="text-xs mb-2 block">DockerエンジンのAPIバージョンを指定します。<br />バージョン1.24以上を指定してください。</span>
                             <Input
                                 label="API Version" placeholder="v1.47" value={"v1.47"} crossOrigin={undefined} />
                         </div>
                     </div>
-                </div>
+                </CardBody>
+                <CardFooter className="flex flex-row-reverse">
+                    <Button color="teal">
+                        保存
+                    </Button>
+                </CardFooter>
             </Card >
         </main>
     )
