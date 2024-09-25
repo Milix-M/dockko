27e31690e4bfddd7dbbfd9da9b4f18a57d588c7d
Milix-M <tujikou12dh@gmail.com>
2024-09-25 15:09:57 +0900

    ウィンドウのChromeをカスタムするように変更

--- desktop/index.js
+++ desktop/index.js
@@ -7,7 +7,12 @@ let win
 
 /** @param {string} url */
 async function createWindow(url) {
-	win = new BrowserWindow({ show: false })
+	win = new BrowserWindow({
+		show: false, titleBarStyle: 'hidden',
+		titleBarOverlay: {
+			height: 40
+		}
+	})
 	await win.loadURL(url)
 	win.show()
 
