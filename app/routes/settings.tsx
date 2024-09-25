6aae7263e8cc90db8501b3b0ae9995ee1eb2e574
Milix-M <tujikou12dh@gmail.com>
2024-09-25 17:13:45 +0900

    入力値をuseStateで保存するように変更

--- app/routes/settings.tsx
+++ app/routes/settings.tsx
@@ -1,5 +1,6 @@
 import { Button, Card, CardBody, CardFooter, CardHeader, Input } from "@material-tailwind/react"
 import { useLoaderData } from "@remix-run/react"
+import React from "react"
 import electron from "~/electron.server"
 
 export function loader() {
@@ -10,6 +11,10 @@ export function loader() {
 
 export default function Settings() {
     const data = useLoaderData<typeof loader>()
+
+    const [dockerEngineServer, setDockerEngineServer] = React.useState("http://127.0.0.1:2375");
+    const [dockerApiVersion, setDockerApiVersion] = React.useState("v1.47")
+
     return (
         <main>
             <h1 className="text-2xl font-bold m-6">Settings</h1>
@@ -28,13 +33,13 @@ export default function Settings() {
                             <span className="font-semibold text-md mb-1 block text-blue-gray-900">Docker Engine Server</span>
                             <span className="text-xs mb-2 block">DockerエンジンのURLを指定します。<br />現在Unix Socketには対応していないためHTTP接続での接続になります。</span>
                             <Input
-                                label="Server URL" placeholder="http://127.0.0.1:2375" value={"http://127.0.0.1:2375"} crossOrigin={undefined} />
+                                label="Server URL" placeholder="http://127.0.0.1:2375" value={dockerEngineServer} crossOrigin={undefined} onChange={(value) => setDockerEngineServer(value.target.value)} />
                         </div>
                         <div className="w-full">
                             <span className="font-semibold text-md mb-1 block text-blue-gray-900">Docker API Version</span>
                             <span className="text-xs mb-2 block">DockerエンジンのAPIバージョンを指定します。<br />バージョン1.24以上を指定してください。</span>
                             <Input
-                                label="API Version" placeholder="v1.47" value={"v1.47"} crossOrigin={undefined} />
+                                label="API Version" placeholder="v1.47" value={dockerApiVersion} crossOrigin={undefined} onChange={(value) => setDockerApiVersion(value.target.value)} />
                         </div>
                     </div>
                 </CardBody>
