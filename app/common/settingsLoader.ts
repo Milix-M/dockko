f5a7321d953f786da55fe6aa005d642d8884fd2a
Milix-M <tujikou12dh@gmail.com>
2024-09-25 17:33:42 +0900

    Local Storagrからデータを読み出す関数を別ファイルに分離

---
+++ app/common/settingsLoader.ts
@@ -0,0 +1,21 @@
+/**
+ * Local Storageに保存したデータを読み込む
+ * @param key 読み出したいキー
+ * @returns 設定値: string, 指定したキーに対応する値が無い場合undefined
+ */
+export default function loadSettingsValue(key: string | undefined | null) {
+    // null or undefinedなら早期リターン
+    if (key == null) {
+        return undefined
+    }
+
+    // Local Storageから値を取ってくる
+    const value = localStorage.getItem(key);
+
+    // nullだと使い勝手悪い時があるのでundefinedにして返す
+    if (value == null) {
+        return undefined
+    } else {
+        return value
+    }
+}
