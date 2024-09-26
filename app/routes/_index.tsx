71808c7153db742f91d9a5d3e6cf348248efe313
Milix-M <tujikou12dh@gmail.com>
2024-09-26 11:23:28 +0900

    Run format, リクエスト先をgetBaseURL()を使用して読み込むように変更

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,70 +1,83 @@
 import { ActionFunctionArgs } from "@remix-run/node";
 import { Suspense } from "react";
 import { useTypedLoaderData } from "remix-typedjson";
-import { DOCKER_ENGINE, DOCKER_ENGINE_VERSION } from "~/common/envs";
+import { getBaseURL } from "~/common/envs";
 import { Container } from "~/common/types/Container";
 import { ContainerDetail } from "~/common/types/ContainerDetail";
 import Table from "~/components/containers/table/Table";
 
 export async function loader() {
-	let containers: Container[] = [];
+  const baseURL = getBaseURL();
 
-	// まず containers を取得
-	const response = await fetch(new URL(DOCKER_ENGINE_VERSION, DOCKER_ENGINE) + "/containers/json?all=true");
-	containers = await response.json();
+  let containers: Container[] = [];
 
-	// コンテナごとの詳細を取得
-	const containerDetails: ContainerDetail[] = await Promise.all(
-		containers.map(async (container) => {
-			const res = await fetch(new URL(DOCKER_ENGINE_VERSION, DOCKER_ENGINE) + `/containers/${container.Id}/json`);
-			return res.json();
-		})
-	);
+  // まず containers を取得
+  const response = await fetch(baseURL + "/containers/json?all=true");
+  containers = await response.json();
 
-	return containerDetails;  // containerDetails を直接返す
+  // コンテナごとの詳細を取得
+  const containerDetails: ContainerDetail[] = await Promise.all(
+    containers.map(async (container) => {
+      const res = await fetch(baseURL + `/containers/${container.Id}/json`);
+      return res.json();
+    })
+  );
+
+  return containerDetails; // containerDetails を直接返す
 }
 
 export async function action({ request }: ActionFunctionArgs) {
-	const formData = await request.formData();
-	const containerId = formData.get('containerId')
-	const containerOperation = formData.get('container')
+  const baseURL = getBaseURL();
 
-	const headers = {
-		'Accept': 'application/json',
-		'Content-Type': 'application/json'
-	};
+  const formData = await request.formData();
+  const containerId = formData.get("containerId");
+  const containerOperation = formData.get("container");
 
-	// nullであってはおかしいので早期return
-	if (containerId == null) {
-		return null
-	}
+  const headers = {
+    Accept: "application/json",
+    "Content-Type": "application/json",
+  };
 
-	let result: Response | null = null;
-	switch (containerOperation) {
-		case "stop":
-			result = await fetch(new URL(DOCKER_ENGINE_VERSION, DOCKER_ENGINE) + `/containers/${containerId}/stop`, { method: 'POST', headers });
-			break;
-		case "start":
-			result = await fetch(new URL(DOCKER_ENGINE_VERSION, DOCKER_ENGINE) + `/containers/${containerId}/start`, { method: 'POST', headers });
-			break;
-		case "trash":
-			result = await fetch(new URL(DOCKER_ENGINE_VERSION, DOCKER_ENGINE) + `/containers/${containerId}?force=true`, { method: 'DELETE', headers });
-			break;
-	}
+  // nullであってはおかしいので早期return
+  if (containerId == null) {
+    return null;
+  }
 
-	return result;
+  let result: Response | null = null;
+  switch (containerOperation) {
+    case "stop":
+      result = await fetch(baseURL + `/containers/${containerId}/stop`, {
+        method: "POST",
+        headers,
+      });
+      break;
+    case "start":
+      result = await fetch(baseURL + `/containers/${containerId}/start`, {
+        method: "POST",
+        headers,
+      });
+      break;
+    case "trash":
+      result = await fetch(baseURL + `/containers/${containerId}?force=true`, {
+        method: "DELETE",
+        headers,
+      });
+      break;
+  }
+
+  return result;
 }
 
 export default function Index() {
-	const containers = useTypedLoaderData<typeof loader>();
+  const containers = useTypedLoaderData<typeof loader>();
 
-	return (
-		<main>
-			<h1 className="text-2xl font-bold m-6">Containers</h1>
+  return (
+    <main>
+      <h1 className="text-2xl font-bold m-6">Containers</h1>
 
-			<Suspense fallback={<div>Loading...</div>}>
-				<Table tableProps={containers} />  {/* コンテナの詳細を表示 */}
-			</Suspense>
-		</main>
-	);
+      <Suspense fallback={<div>Loading...</div>}>
+        <Table tableProps={containers} /> {/* コンテナの詳細を表示 */}
+      </Suspense>
+    </main>
+  );
 }
