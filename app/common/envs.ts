86bfa1a70a1fe998193a8b7934ef916ca324220e
Milix-M <tujikou12dh@gmail.com>
2024-09-26 11:21:27 +0900

    接続に関する設定値を取得して返す関数を作成, リクエスト先になるベースURLを作成して返却する関数を作成

--- app/common/envs.ts
+++ app/common/envs.ts
@@ -1,2 +1,34 @@
-export const DOCKER_ENGINE = "http://127.0.0.1:2375"
-export const DOCKER_ENGINE_VERSION = "v1.47"
+import { store } from "~/electron.server";
+
+/**
+ * 接続に関する値をelectron-storeを使用して読み込む
+ * @returns DockerエンジンのURLとAPIバージョン
+ */
+export function loadConnectSettingValues() {
+  const DOCKER_ENGINE = store.get(
+    "dockerEngineServer",
+    "http://127.0.0.1:2375"
+  ) as string;
+  const DOCKER_ENGINE_VERSION = store.get(
+    "dockerApiVersion",
+    "v1.47"
+  ) as string;
+
+  return {
+    DOCKER_ENGINE,
+    DOCKER_ENGINE_VERSION,
+  };
+}
+
+/**
+ * リクエスト先のURLを返却する
+ * @returns リクエスト先URL
+ */
+export function getBaseURL() {
+  const connectSettings = loadConnectSettingValues();
+
+  return new URL(
+    connectSettings.DOCKER_ENGINE_VERSION,
+    connectSettings.DOCKER_ENGINE
+  );
+}
