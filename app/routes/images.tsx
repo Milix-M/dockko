de4d248e29bf1ed7e47c16e58be6c791de8f463e
Milix-M <tujikou12dh@gmail.com>
2024-09-26 14:18:18 +0900

    不要なインポート& 不要なコード削除

--- app/routes/images.tsx
+++ app/routes/images.tsx
@@ -1,17 +1,15 @@
-import { useLoaderData } from "@remix-run/react"
-import electron from "~/electron.server"
+import electron from "~/electron.server";
 
 export function loader() {
-    return {
-        userDataPath: electron.app.getPath("userData"),
-    }
+  return {
+    userDataPath: electron.app.getPath("userData"),
+  };
 }
 
 export default function Images() {
-    const data = useLoaderData<typeof loader>()
-    return (
-      <main>
-        <h1 className="text-2xl font-bold m-6">Images</h1>
-      </main>
-    );
+  return (
+    <main>
+      <h1 className="text-2xl font-bold m-6">Images</h1>
+    </main>
+  );
 }
