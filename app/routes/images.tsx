a4239e7229884a05693a50d547e3d279f95ab32c
Milix-M <tujikou12dh@gmail.com>
2024-09-26 17:41:23 +0900

    検索システムを追加

--- app/routes/images.tsx
+++ app/routes/images.tsx
@@ -1,9 +1,10 @@
+import { LoaderFunctionArgs } from "@remix-run/node";
 import { useTypedLoaderData } from "remix-typedjson";
 import { getBaseURL } from "~/common/envs";
 import { Image } from "~/common/types/image/Image";
 import Table from "~/components/images/table/Table";
 
-export async function loader() {
+export async function loader({ request }: LoaderFunctionArgs) {
   const baseURL = getBaseURL();
 
   let images: Image[] = [];
@@ -18,7 +19,45 @@ export async function loader() {
     return null;
   }
 
-  return images;
+  // クエリを取り出す
+  const url = new URL(request.url);
+  const searchParams = url.searchParams;
+
+  // 検索Boxの入力値をURLクエリから取得
+  const searchWord = searchParams.get("search");
+
+  // 検索キーワード無ければ絞り込みしない
+  if (searchWord == null) {
+    return images;
+  }
+
+  // 検索boxの入力値でフィルタリングしたイメージ配列
+  const filteredImages: Image[] = [];
+
+  for (const image of images) {
+    // RepoTagsが存在してundefinedでないのを確認
+    if (image.RepoTags.length >= 1 && image.RepoTags[0] != null) {
+      const imageName = image.RepoTags[0].split(":")[0];
+      const tagName = image.RepoTags[0].split(":").at(-1);
+
+      // イメージ名と一致していたら
+      if (
+        imageName != null &&
+        imageName.toLocaleLowerCase().includes(searchWord.toLocaleLowerCase())
+      ) {
+        filteredImages.push(image);
+        // タグ名と一致していたら
+      } else if (
+        tagName != null &&
+        tagName.toLocaleLowerCase().includes(searchWord.toLocaleLowerCase())
+      ) {
+        filteredImages.push(image);
+      }
+    }
+
+    // フィルタリングしたimageの配列を返却
+    return filteredImages;
+  }
 }
 
 export default function Images() {
